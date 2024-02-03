98f43cf98b0be4ba6175e38aad7a26118a039976
w4t3rcs <uimanovmaks@gmail.com>
2024-02-03 19:52:39 +0100

    Added utils for world features

---
+++ src/main/java/com/temporal/api/core/util/feature/ConfiguredFeatureUtils.java
@@ -0,0 +1,19 @@
+package com.temporal.api.core.util.feature;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
+
+public class ConfiguredFeatureUtils {
+    public static ResourceKey<ConfiguredFeature<?, ?>> createKey(String name, String modId) {
+        return ResourceKey.create(Registries.CONFIGURED_FEATURE, new ResourceLocation(modId, name));
+    }
+
+    public static <FC extends FeatureConfiguration, F extends Feature<FC>> void register(BootstapContext<ConfiguredFeature<?, ?>> context, ResourceKey<ConfiguredFeature<?, ?>> key, F feature, FC configuration) {
+        context.register(key, new ConfiguredFeature<>(feature, configuration));
+    }
+}
---
+++ src/main/java/com/temporal/api/core/util/feature/PlacedFeatureUtils.java
@@ -0,0 +1,26 @@
+package com.temporal.api.core.util.feature;
+
+import net.minecraft.core.Holder;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+import net.minecraft.world.level.levelgen.placement.PlacementModifier;
+
+import java.util.List;
+
+public class PlacedFeatureUtils {
+    public static ResourceKey<PlacedFeature> createKey(String name, String modId) {
+        return ResourceKey.create(Registries.PLACED_FEATURE, new ResourceLocation(modId, name));
+    }
+
+    public static void register(BootstapContext<PlacedFeature> context, ResourceKey<PlacedFeature> key, Holder<ConfiguredFeature<?, ?>> configuration, List<PlacementModifier> modifiers) {
+        context.register(key, new PlacedFeature(configuration, List.copyOf(modifiers)));
+    }
+
+    public static void register(BootstapContext<PlacedFeature> context, ResourceKey<PlacedFeature> key, Holder<ConfiguredFeature<?, ?>> configuration, PlacementModifier... modifiers) {
+        register(context, key, configuration, List.of(modifiers));
+    }
+}
