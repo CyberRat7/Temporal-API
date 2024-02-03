f8f493df9389a533245cfb15aef2d931f755e524
w4t3rcs <uimanovmaks@gmail.com>
2024-02-03 12:56:30 +0100

    Initialization of Gradle and default properties

---
+++ src/main/java/com/temporal/api/config/ApiConfig.java
@@ -0,0 +1,17 @@
+package com.temporal.api.config;
+
+import com.temporal.api.ApiMod;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.config.ModConfigEvent;
+
+@Mod.EventBusSubscriber(modid = ApiMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ApiConfig {
+    private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+    public static final ForgeConfigSpec SPEC = BUILDER.build();
+
+    @SubscribeEvent
+    static void onLoad(final ModConfigEvent event) {
+    }
+}
