cd308114813760a7211c618dfb7795387b96a9c2
w4t3rcs <uimanovmaks@gmail.com>
2024-02-03 19:48:54 +0100

    Added first CustomProperties (Bows, Crossbows, Shields, etc need  their custom props)

---
+++ src/main/java/com/temporal/api/core/properties/CustomItemProperties.java
@@ -0,0 +1,38 @@
+package com.temporal.api.core.properties;
+
+import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CrossbowItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.ComposterBlock;
+
+public class CustomItemProperties {
+    private static void putCompostable(ItemLike itemLike, float value) {
+        ComposterBlock.COMPOSTABLES.put(itemLike, value);
+    }
+
+    private static void makeBow(Item item) {
+        ItemProperties.register(item, new ResourceLocation("pull"), (itemStack, clientLevel, livingEntity, number) -> {
+            if (livingEntity == null) return 0.0F;
+            else return livingEntity.getUseItem() != itemStack ? 0.0F : (float) (itemStack.getUseDuration() - livingEntity.getUseItemRemainingTicks()) / 20.0F;
+        });
+
+        ItemProperties.register(item, new ResourceLocation("pulling"), (itemStack, clientLevel, livingEntity, number) -> livingEntity != null && livingEntity.isUsingItem() && livingEntity.getUseItem() == itemStack ? 1.0F : 0.0F);
+    }
+
+    private static void makeCrossbow(Item item) {
+        ItemProperties.register(item, new ResourceLocation("pull"), (itemStack, level, entity, seed) -> {
+            if (entity == null) return 0.0F;
+            else return CrossbowItem.isCharged(itemStack) ? 0.0F : (float)(itemStack.getUseDuration() - entity.getUseItemRemainingTicks()) / (float)CrossbowItem.getChargeDuration(itemStack);
+        });
+        ItemProperties.register(item, new ResourceLocation("pulling"), (itemStack, level, entity, seed) -> entity != null && entity.isUsingItem() && entity.getUseItem() == itemStack && !CrossbowItem.isCharged(itemStack) ? 1.0F : 0.0F);
+        ItemProperties.register(item, new ResourceLocation("charged"), (itemStack, level, entity, seed) -> entity != null && CrossbowItem.isCharged(itemStack) ? 1.0F : 0.0F);
+        ItemProperties.register(item, new ResourceLocation("firework"), (itemStack, level, entity, seed) -> entity != null && CrossbowItem.isCharged(itemStack) && CrossbowItem.containsChargedProjectile(itemStack, Items.FIREWORK_ROCKET) ? 1.0F : 0.0F);
+    }
+
+    private static void makeShield(Item item) {
+        ItemProperties.register(item, new ResourceLocation( "blocking"), (itemStack, clientLevel, livingEntity, number) -> livingEntity != null && livingEntity.isUsingItem() && livingEntity.getUseItem() == itemStack ? 1.0F : 0.0F);
+    }
+}
