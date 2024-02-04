7224b21593b96aadfc79ec76383abf9da5a346ef
w4t3rcs <uimanovmaks@gmail.com>
2024-02-04 23:40:27 +0100

    ParticleFactory returns new RegistryObject<ParticleType<SimpleParticleType>> (before it was RegistryObject<ParticleType<?>>)

--- src/main/java/com/temporal/api/core/registry/factory/common/ParticleFactory.java
+++ src/main/java/com/temporal/api/core/registry/factory/common/ParticleFactory.java
@@ -7,24 +7,24 @@ import net.minecraftforge.registries.RegistryObject;
 
 import java.util.function.Supplier;
 
-public abstract class ParticleFactory implements TypedFactory<ParticleType<?>> {
+public abstract class ParticleFactory implements TypedFactory<ParticleType<SimpleParticleType>> {
     private final DeferredRegister<ParticleType<?>> particleRegister;
 
     public ParticleFactory(DeferredRegister<ParticleType<?>> particleRegister) {
         this.particleRegister = particleRegister;
     }
 
-    public RegistryObject<ParticleType<?>> create(String name, boolean overrideLimiter) {
+    public RegistryObject<ParticleType<SimpleParticleType>> create(String name, boolean overrideLimiter) {
         return particleRegister.register(name, particleRegister.register(name, () -> new SimpleParticleType(overrideLimiter)));
     }
 
     @Override
-    public RegistryObject<ParticleType<?>> create(String name, Supplier<ParticleType<?>> particleTypeSupplier) {
+    public RegistryObject<ParticleType<SimpleParticleType>> create(String name, Supplier<ParticleType<SimpleParticleType>> particleTypeSupplier) {
         return particleRegister.register(name, particleTypeSupplier);
     }
 
     @Override
-    public RegistryObject<? extends ParticleType<?>> createTyped(String name, Supplier<? extends ParticleType<?>> tSupplier) {
+    public RegistryObject<? extends ParticleType<SimpleParticleType>> createTyped(String name, Supplier<? extends ParticleType<SimpleParticleType>> tSupplier) {
         return particleRegister.register(name, tSupplier);
     }
 }
