83045bf07324f8ee4b72fa3d29bfca7ff1cb1c41
w4t3rcs <uimanovmaks@gmail.com>
2024-02-04 17:09:33 +0100

    Added BowExtension

---
+++ src/main/java/com/temporal/api/core/registry/factory/extension/item/BowExtension.java
@@ -0,0 +1,22 @@
+package com.temporal.api.core.registry.factory.extension.item;
+
+import com.temporal.api.core.registry.factory.common.TypedFactory;
+import net.minecraft.world.item.BowItem;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
+@SuppressWarnings("unchecked")
+public interface BowExtension {
+    /**
+     * @param args = args[0] - item properties
+     */
+    default RegistryObject<BowItem> createArrow(String name, TypedFactory<Item> tTypedFactory, Object... args) {
+        return (RegistryObject<BowItem>) tTypedFactory.createTyped(name, () -> new BowItem((Item.Properties) args[0]));
+    }
+
+    default RegistryObject<? extends BowItem> createArrow(String name, TypedFactory<Item> tTypedFactory, Supplier<? extends BowItem> tTypedSupplier) {
+        return (RegistryObject<? extends BowItem>) tTypedFactory.createTyped(name, tTypedSupplier);
+    }
+}
