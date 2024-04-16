8b9267b661cf3516f690b670498a825137862ec1
w4t3rcs <uimanovmaks@gmail.com>
2024-04-16 15:37:14 +0200

    Added BoatExtension

--- src/main/java/com/temporal/api/core/registry/factory/common/EntityTypeFactory.java
+++ src/main/java/com/temporal/api/core/registry/factory/common/EntityTypeFactory.java
@@ -1,5 +1,6 @@
 package com.temporal.api.core.registry.factory.common;
 
+import com.temporal.api.client.dto.Size;
 import com.temporal.api.core.engine.event.registry.EnginedRegisterFactory;
 import com.temporal.api.core.engine.metadata.context.InjectionContext;
 import net.minecraft.core.registries.Registries;
@@ -10,20 +11,18 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
-import java.awt.*;
 import java.util.function.Supplier;
 
 public class EntityTypeFactory implements TypedFactory<EntityType<?>> {
     public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = EnginedRegisterFactory.create(Registries.ENTITY_TYPE);
 
-    public <T extends Entity> RegistryObject<EntityType<?>> create(String name, EntityType.EntityFactory<T> entityFactory, Rectangle rectangle, MobCategory category) {
-        return ENTITY_TYPES.register(name, () -> EntityType.Builder.of(entityFactory, category)
-                .sized((float) rectangle.getWidth(), (float) rectangle.getHeight())
-                .build(name));
+    public <T extends Entity> RegistryObject<EntityType<?>> create(String name, EntityType.EntityFactory<T> entityFactory, Size size, MobCategory category) {
+        return this.create(name, EntityType.Builder.of(entityFactory, category)
+                .sized(size.width(), size.height()));
     }
 
     public RegistryObject<EntityType<?>> create(String name, EntityType.Builder<?> builder) {
-        return ENTITY_TYPES.register(name, () -> builder.build(name));
+        return this.create(name, () -> builder.build(name));
     }
 
     @Override
