e0e6e4764a51ba15690a66fc8f30ecd17a197dfe
w4t3rcs <uimanovmaks@gmail.com>
2024-04-16 15:24:34 +0200

    Extended version of EntityTypeFactory

--- src/main/java/com/temporal/api/core/registry/factory/common/EntityTypeFactory.java
+++ src/main/java/com/temporal/api/core/registry/factory/common/EntityTypeFactory.java
@@ -3,15 +3,30 @@ package com.temporal.api.core.registry.factory.common;
 import com.temporal.api.core.engine.event.registry.EnginedRegisterFactory;
 import com.temporal.api.core.engine.metadata.context.InjectionContext;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.awt.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.function.Supplier;
 
 public class EntityTypeFactory implements TypedFactory<EntityType<?>> {
     public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = EnginedRegisterFactory.create(Registries.ENTITY_TYPE);
+
+    public <T extends Entity> RegistryObject<EntityType<?>> create(String name, EntityType.EntityFactory<T> entityFactory, Rectangle rectangle, MobCategory category) {
+        return ENTITY_TYPES.register(name, () -> EntityType.Builder.of(entityFactory, category)
+                .sized((float) rectangle.getWidth(), (float) rectangle.getHeight())
+                .build(name));
+    }
+
+    public RegistryObject<EntityType<?>> create(String name, EntityType.Builder<?> builder) {
+        return ENTITY_TYPES.register(name, () -> builder.build(name));
+    }
 
     @Override
     public RegistryObject<EntityType<?>> create(String name, Supplier<EntityType<?>> entitySupplier) {
