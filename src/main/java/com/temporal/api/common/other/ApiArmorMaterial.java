1e3446e8fa789d6f9f3745c54960df8eeb25d62e
w4t3rcs <w4t3rofficial@gmail.com>
2024-06-30 00:59:56 +0200

    Added ApiArmorMaterial
    ApiArmorMaterial implements ArmorMaterial and the main goal of this class is to make creating of armor materials as easy as it is possible

---
+++ src/main/java/com/temporal/api/common/other/ApiArmorMaterial.java
@@ -0,0 +1,91 @@
+package com.temporal.api.common.other;
+
+import com.temporal.api.core.engine.IOLayer;
+import net.minecraft.Util;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.util.LazyLoadedValue;
+import net.minecraft.util.StringRepresentable;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.ArmorMaterials;
+import net.minecraft.world.item.crafting.Ingredient;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.EnumMap;
+import java.util.function.Supplier;
+
+public class ApiArmorMaterial implements StringRepresentable, ArmorMaterial {
+    public static final StringRepresentable.EnumCodec<ArmorMaterials> CODEC = StringRepresentable.fromEnum(ArmorMaterials::values);
+    private static final EnumMap<ArmorItem.Type, Integer> HEALTH_FUNCTION_FOR_TYPE = Util.make(new EnumMap<>(ArmorItem.Type.class), map -> {
+        map.put(ArmorItem.Type.BOOTS, 13);
+        map.put(ArmorItem.Type.LEGGINGS, 15);
+        map.put(ArmorItem.Type.CHESTPLATE, 16);
+        map.put(ArmorItem.Type.HELMET, 11);
+    });
+    private final String modId;
+    private final String name;
+    private final int durabilityMultiplier;
+    private final EnumMap<ArmorItem.Type, Integer> protectionFunctionForType;
+    private final int enchantmentValue;
+    private final SoundEvent sound;
+    private final float toughness;
+    private final float knockbackResistance;
+    private final LazyLoadedValue<Ingredient> repairIngredient;
+
+    public ApiArmorMaterial(String name, int durabilityMultiplier, EnumMap<ArmorItem.Type, Integer> protectionFunctionForType, int enchantmentValue, SoundEvent sound, float toughness, float knockbackResistance, Supplier<Ingredient> repairIngredient) {
+        this(IOLayer.FORGE_MOD.getModId(), name, durabilityMultiplier, protectionFunctionForType, enchantmentValue, sound, toughness, knockbackResistance, repairIngredient);
+    }
+
+    public ApiArmorMaterial(String modId, String name, int durabilityMultiplier, EnumMap<ArmorItem.Type, Integer> protectionFunctionForType, int enchantmentValue, SoundEvent sound, float toughness, float knockbackResistance, Supplier<Ingredient> repairIngredient) {
+        this.modId = modId;
+        this.name = name;
+        this.durabilityMultiplier = durabilityMultiplier;
+        this.protectionFunctionForType = protectionFunctionForType;
+        this.enchantmentValue = enchantmentValue;
+        this.sound = sound;
+        this.toughness = toughness;
+        this.knockbackResistance = knockbackResistance;
+        this.repairIngredient = new LazyLoadedValue<>(repairIngredient);
+    }
+
+    @Override
+    @NotNull
+    public String getSerializedName() {
+        return this.getName();
+    }
+
+    public int getDurabilityForType(@NotNull ArmorItem.Type type) {
+        return HEALTH_FUNCTION_FOR_TYPE.get(type) * this.durabilityMultiplier;
+    }
+
+    public int getDefenseForType(@NotNull ArmorItem.Type type) {
+        return this.protectionFunctionForType.get(type);
+    }
+
+    public int getEnchantmentValue() {
+        return this.enchantmentValue;
+    }
+
+    @NotNull
+    public SoundEvent getEquipSound() {
+        return this.sound;
+    }
+
+    @NotNull
+    public Ingredient getRepairIngredient() {
+        return this.repairIngredient.get();
+    }
+
+    @NotNull
+    public String getName() {
+        return this.modId + ":" + this.name;
+    }
+
+    public float getToughness() {
+        return this.toughness;
+    }
+
+    public float getKnockbackResistance() {
+        return this.knockbackResistance;
+    }
+}
